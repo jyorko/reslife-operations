aa8ccaa418e6eabaf238bb5148b33e76fed74f14
ZSabakh <zsabakhtarishvili@atu.edu>
2023-11-27 20:49:48 -0600

    Added shifts controller, can fetch shifts

--- node/src/utility/shift.query.helpers.ts
+++ node/src/utility/shift.query.helpers.ts
@@ -3,6 +3,35 @@ import { TShiftPreview } from "../models/shift.model";
 import { Types } from "mongoose";
 
 class ShiftQueryHelper {
+  public static appendUserIDFilter(userID: string, query: Record<string, any>) {
+    if (userID) {
+      query.userID = userID;
+    }
+  }
+
+  public static appendPeriodFilter(period_from: string, period_to: string, query: Record<string, any>) {
+    if (period_from && period_to) {
+      query.$and = [
+        {
+          $or: [
+            {
+              startTime: {
+                $gte: new Date(period_from),
+                $lte: new Date(period_to),
+              },
+            },
+            {
+              endTime: {
+                $gte: new Date(period_from),
+                $lte: new Date(period_to),
+              },
+            },
+          ],
+        },
+      ];
+    }
+  }
+
   public static getPreviewFields(): string {
     const previewObj = {
       _id: "",
