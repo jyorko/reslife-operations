37287ed317250f0915efb14ca80d4981d925e05f
Saori Kojima <skojima@atu.edu>
2024-02-05 16:00:25 -0600

    finished API call of user Info with fixed index

--- flutter/app/lib/API/api_operation.dart
+++ flutter/app/lib/API/api_operation.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 // fetch user info
 Future<Map<String, dynamic>> fetchData() async {
   final response = await http
-      .get(Uri.parse("https://reslife-operations.vercel.app/api/proxy/"));
+      .get(Uri.parse("http://10.60.170.18/api/v1/student_staff?page=1"));
   if (response.statusCode == 200) {
     return json.decode(response.body);
   } else {
