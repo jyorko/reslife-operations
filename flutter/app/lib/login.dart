ff8162b806d12d263b4a2b516201bfacedcaa018
Saori Kojima <skojima@atu.edu>
2024-02-19 21:36:08 -0600

    added test line for cookie

--- flutter/app/lib/login.dart
+++ flutter/app/lib/login.dart
@@ -44,7 +44,9 @@ class _LoginState extends State<Login> {
       );
 
       // Extract the cookie from the response headers
-      String? setCookieHeader = response.headers.value('Set-Cookie');
+      // NOTE: upper line has an issue, need to be fixed
+      //String? setCookieHeader = response.headers.value('Set-Cookie');
+      String? setCookieHeader = 'Test';
 
       // Check if the authentication was successful
       if (response.statusCode == 200 && setCookieHeader != null) {
